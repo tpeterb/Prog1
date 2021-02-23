52e45970b4aef68538ce9aaa64c56541f2fa1bcd
tpeterb <peter.balazs.toth@gmail.com>
2021-02-23 17:19:10 +0100

    Create Chapter_12_Excercise_9.cpp
    A kép megjelenítéséhez kell a Snowfall.jpg állomány.

---
+++ Chapter_12_Excercise_9.cpp
@@ -0,0 +1,16 @@
+#include "Simple_window.h" // get access to our window library
+#include "Graph.h" // get access to our graphics library facilities
+
+int main()
+{
+	using namespace Graph_lib;
+	Point p{ 200, 200 };
+	Simple_window win(p, 1200, 800, "My window");
+	Image img(Point(500, 500), "Snowfall.jpg");
+	Text t(Point(600, 700), "Snowfall");
+	t.set_color(Color::dark_blue);
+	win.label("Snowfall");
+	win.attach(img);
+	win.attach(t);
+	win.wait_for_button();
+}

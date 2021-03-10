863ae551e5ecf39b3bb5beef07ff84151e3fff0e
tpeterb <peter.balazs.toth@gmail.com>
2021-03-10 12:04:59 +0100

    Update Ch_13_Drill.cpp

--- Ch_13_Drill.cpp
+++ Ch_13_Drill.cpp
@@ -7,18 +7,18 @@ int main()
 	constexpr int xstart = 0, ystart = 0;
 	int cy = 0, dx = 0, l = 0;
 	Point p{ xstart, ystart };
-	Simple_window win(p, 1000, 800, "My window"); //1. feladat
-	Graph_lib::Rectangle *r;
+	Simple_window win(p, 800, 1000, "My window"); //1. feladat
+	Vector_ref<Graph_lib::Rectangle> v;
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			r = new Graph_lib::Rectangle(Point(xstart+dx, ystart+cy), 100, 100);
-			if(xstart+dx == ystart+cy) //3. feladat
+			v.push_back(new Graph_lib::Rectangle(Point(xstart + dx, ystart + cy), 100, 100));
+			if (xstart + dx == ystart + cy) //3. feladat
 			{
-				r->set_fill_color(Color::red);
+				v[v.size()-1].set_fill_color(Color::red);
 			}
-			win.attach(*r);
+			win.attach(v[v.size() - 1]);
 			dx += 100;
 		}
 		l++;
@@ -38,6 +38,10 @@ int main()
 		for (int j = 0; j < 8; j++)
 		{
 			win.wait_for_button();
+			if (j == 7 && i == 7) //Ne menjen a négyzetrács alá
+			{
+				break;
+			}
 			cool.move(100, 0);
 		}
 		cool.move(-800, 100);
